ab357f66d90725e8f2c1c7911104d61907a00088
Timothy Chandler <tim@geomash.com>
2016-05-31 19:31:46 +0800

    Fixed support for NodeJS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,19 @@
 module.exports = function(grunt)
 {
-	var BANNER=	"/*\r\n"
-				+' JSKK v<%= pkg.version %>'
-				+' | (c) 2014 Timothy Chandler <tim@geomash.com>'
-				+' | http://github.com/melechi/JS-Klass-Kit/blob/master/LICENSE '
-				+"\r\n"
-				+' Date Built: <%= grunt.template.today("yyyy-mm-dd") %>'
-				+"\r\n*/\r\n";
+	var	BANNER=
+		[
+			"/*\r\n",
+			' JSKK v<%= pkg.version %>',
+			' | (c) 2014 Timothy Chandler <tim@geomash.com>',
+			' | http://github.com/melechi/JS-Klass-Kit/blob/master/LICENSE ',
+			"\r\n",
+			' Date Built: <%= grunt.template.today("yyyy-mm-dd") %>',
+			"\r\n*/\r\n",
+			"\r\n",
+		].join(''),
+		nodeJSFix="if (typeof define!=='function'){var define=require('amdefine')(module);}\r\n";
 	
+
 	// Project configuration.
 	grunt.initConfig
 	(
@@ -24,8 +30,8 @@ module.exports = function(grunt)
 					files:
 					[
 						{
-							src:	'bin/<%= pkg.name %>.<%= pkg.version %>.js',
-							dest:	'bin/<%= pkg.name %>.<%= pkg.version %>.min.js'
+							src:	'bin/<%= pkg.name %>.js',
+							dest:	'bin/<%= pkg.name %>.min.js'
 						}
 					]
 				}
@@ -37,13 +43,13 @@ module.exports = function(grunt)
 					files:
 					[
 						{
-							src: 	'bin/<%= pkg.name %>.<%= pkg.version %>.js',
-							dest: 	'bin/<%= pkg.name %>.js',
+							src: 	'bin/<%= pkg.name %>.js',
+							dest: 	'bin/<%= pkg.name %>.<%= pkg.version %>.js',
 							filter: 'isFile'
 						},
 						{
-							src: 	'bin/<%= pkg.name %>.<%= pkg.version %>.min.js',
-							dest: 	'bin/<%= pkg.name %>.min.js',
+							src: 	'bin/<%= pkg.name %>.min.js',
+							dest: 	'bin/<%= pkg.name %>.<%= pkg.version %>.min.js',
 							filter: 'isFile'
 						}
 					]
@@ -55,15 +61,27 @@ module.exports = function(grunt)
 				{
 					options:
 					{
-						stripBanners:	true,
-						banner:			BANNER,
 						baseUrl:		"src/",
 						name:			"JSKK",
 						optimize:		"none",
 						shim:			grunt.file.readJSON('Shim.json'),
 						wrapShim:		true,
-						out:			"bin/<%= pkg.name %>.<%= pkg.version %>.js"
+						out:			"bin/<%= pkg.name %>.js"
 					}
+				}
+			},
+			file_append:
+			{
+				default_options:
+				{
+					files:
+					[
+						{
+							prepend:	nodeJSFix,
+							input:		'bin/<%= pkg.name %>.js',
+							output:		'bin/<%= pkg.name %>.js'
+						}
+					]
 				}
 			}
 		}
@@ -72,7 +90,9 @@ module.exports = function(grunt)
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
 	grunt.loadNpmTasks('grunt-contrib-copy');
-
-	grunt.registerTask('default', ['requirejs','uglify','copy']);
+	grunt.loadNpmTasks('grunt-file-append');
+	
+	// grunt.registerTask('default', ['requirejs','uglify','copy']);
+	grunt.registerTask('default', ['requirejs','file_append','uglify','copy']);
 
 };
